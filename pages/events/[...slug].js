a3a4b0ce8332312684d9c4f8c3fb2d6c74fb824d
tpgus <tpgus758@naver.com>
2023-01-03 11:06:43 +0900

    실습 프로젝트

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,19 +1,37 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
-import { getFilteredEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import ResultsTitle from '../../components/events/results-title';
-import Button from '../../components/ui/button';
-import ErrorAlert from '../../components/ui/error-alert';
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
 
   const filterData = router.query.slug;
 
-  if (!filterData) {
-    return <p className='center'>Loading...</p>;
+  const { data, error } = useSWR(
+    "https://react-http-323ac-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  );
+
+  useEffect(() => {
+    if (data) {
+      const events = [];
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
   }
 
   const filteredYear = filterData[0];
@@ -28,23 +46,27 @@ function FilteredEventsPage() {
     numYear > 2030 ||
     numYear < 2021 ||
     numMonth < 1 ||
-    numMonth > 12
+    numMonth > 12 ||
+    error
   ) {
     return (
       <Fragment>
         <ErrorAlert>
           <p>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
       </Fragment>
     );
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
@@ -53,8 +75,8 @@ function FilteredEventsPage() {
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
       </Fragment>
     );
@@ -71,3 +93,47 @@ function FilteredEventsPage() {
 }
 
 export default FilteredEventsPage;
+
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+
+//   const filterData = params.slug;
+
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
+
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
+
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     return {
+//       // notFound: true,
+//       //방법 1 : props로 전달하고 컴포넌트에서 처리
+//       props: {
+//         hasError: true,
+//       },
+//       //방법 2
+//       // redirect:{
+//       //   destination:'/error-page'
+//       // }
+//       //방법 3
+//       //notFound처리
+//     };
+//   }
+
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
+
+//   return {
+//     props: { events: filteredEvents, date: { year: numYear, month: numMonth } },
+//   };
+// }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,17 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
-import { getAllEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import EventsSearch from '../../components/events/events-search';
+import { getAllEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
-    
+
     router.push(fullPath);
   }
 
@@ -24,3 +24,14 @@ function AllEventsPage() {
 }
 
 export default AllEventsPage;
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
